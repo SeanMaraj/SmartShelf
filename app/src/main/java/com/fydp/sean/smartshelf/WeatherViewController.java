7075081980d6595dd842737a34c7820e429ea51b
SEAN MARAJ <stmaraj@uwaterloo.ca>
2015-07-12 20:56:01 -0400

    Finished Navigation Controller

---
+++ app/src/main/java/com/fydp/sean/smartshelf/WeatherViewController.java
@@ -0,0 +1,23 @@
+package com.fydp.sean.smartshelf;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Sean on 2015-07-12.
+ */
+public class WeatherViewController extends Fragment {
+
+    View rootView = null;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.view_weather, container, false);
+        return rootView;
+    }
+}
