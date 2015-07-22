931baad8950a88f0af4b3ce6bd33be143478833d
SEAN MARAJ <stmaraj@uwaterloo.ca>
2015-07-22 00:19:43 -0400

    -Added DataFetcher -manipulated weather view for testing DataFetcher

--- app/src/main/java/com/fydp/sean/smartshelf/Controllers/WeatherViewController.java
+++ app/src/main/java/com/fydp/sean/smartshelf/Controllers/WeatherViewController.java
@@ -6,8 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.fydp.sean.smartshelf.DataFetcher;
 import com.fydp.sean.smartshelf.R;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Sean on 2015-07-12.
@@ -20,6 +24,28 @@ public class WeatherViewController extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.view_weather, container, false);
+
+        getData();
+
         return rootView;
+    }
+
+    private void getData() {
+
+        TextView view = (TextView)rootView.findViewById(R.id.weatherText);
+        String result = "";
+        String login_url = "http://10.0.2.2/webapp/login.php";
+
+        DataFetcher df = new DataFetcher();
+
+        try {
+            result = df.execute(login_url).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        view.setText(result);
     }
 }
---
+++ app/src/main/java/com/fydp/sean/smartshelf/DataFetcher.java
@@ -0,0 +1,39 @@
+package com.fydp.sean.smartshelf;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Sean on 2015-07-21.
+ */
+public class DataFetcher extends AsyncTask<String, Void, String> {
+
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        BufferedReader reader = null;
+
+        try{
+            URL url = new URL(params[0]);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            StringBuilder sb = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+
+            return sb.toString();
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
