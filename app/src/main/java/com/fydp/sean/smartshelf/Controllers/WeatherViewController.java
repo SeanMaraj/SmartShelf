e8f32f4a7f442a17e51d678b47debe5e24cb3068
SeanMaraj <seanmaraj@live.com>
2015-07-23 18:50:43 -0400

    -zone list now gets real data -zone item initial weight now wraps content

--- app/src/main/java/com/fydp/sean/smartshelf/Controllers/WeatherViewController.java
+++ app/src/main/java/com/fydp/sean/smartshelf/Controllers/WeatherViewController.java
@@ -11,6 +11,12 @@ import android.widget.TextView;
 import com.fydp.sean.smartshelf.DataFetcher;
 import com.fydp.sean.smartshelf.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -33,7 +39,8 @@ public class WeatherViewController extends Fragment {
     private void getData() {
 
         TextView view = (TextView)rootView.findViewById(R.id.weatherText);
-        String url = "http://ip.jsontest.com/";
+        //String url = "http://ip.jsontest.com/";
+        String url = "http://99.236.1.225:5001/getzones/1";
         String result = "";
 
         DataFetcher df = new DataFetcher();
@@ -46,6 +53,28 @@ public class WeatherViewController extends Fragment {
             e.printStackTrace();
         }
 
+
+        //result = "[{\"id\": 1, \"baseid\": 1, \"type\": \"scale\", \"weight\": \"0.000000\", \"units\": \"grams\"}, {\"id\": 2, \"baseid\": 1, \"type\": \"scale\", \"weight\": \"0.000000\", \"units\": \"grams\"}, {\"id\": 3, \"baseid\": 1, \"type\": \"scale\", \"weight\": \"0.132450\", \"units\": \"grams\"}, {\"id\": 4, \"baseid\": 1, \"type\": \"scale\", \"weight\": \"0.000000\", \"units\": \"grams\"}]";
+
+        parseResult(result);
+
         view.setText(result);
+    }
+
+
+    private void parseResult(String result) {
+        try{
+            JSONArray zones = new JSONArray(result);
+            ArrayList<Integer> ids = new ArrayList<Integer>();
+
+            for (int i=0; i<zones.length(); i++)
+            {
+                ids.add(zones.getJSONObject(i).getInt("id"));
+            }
+
+
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/fydp/sean/smartshelf/Controllers/ZonesViewController.java
+++ app/src/main/java/com/fydp/sean/smartshelf/Controllers/ZonesViewController.java
@@ -6,11 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.fydp.sean.smartshelf.Adaptors.ZoneAdaptor;
+import com.fydp.sean.smartshelf.DataFetcher;
 import com.fydp.sean.smartshelf.Models.ZoneModel;
 import com.fydp.sean.smartshelf.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Sean on 2015-07-12.
@@ -21,28 +30,101 @@ public class ZonesViewController extends Fragment {
     ListView zoneListView;
     ZoneAdaptor adaptor;
 
+    ArrayList<Integer> zoneNumbers = new ArrayList<Integer>();
+    ArrayList<String> itemNames = new ArrayList<String>();
+    ArrayList<Integer> percentages = new ArrayList<Integer>();
+    ArrayList<Integer> initialWeights = new ArrayList<Integer>();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.view_zones, container, false);
         zoneListView = (ListView)rootView.findViewById(R.id.zoneList);
 
-        //TODO: get data from database
-        int[] zoneNumbers = {1,2,3,4};
+/*
+        int[] zoneNumberss = {1,2,3,4};
         String[] itemNames = {"Sugar", "Flour", "Bolts", "Nuts"};
         int[] percentages = {25,8,80,50};
         int[] initialWeights = {100, 200, 62, 400};
+*/
 
+        getData();
 
         adaptor = new ZoneAdaptor(getActivity(), R.layout.row_zone);
         zoneListView.setAdapter(adaptor);
-
-        for (int i=0; i<zoneNumbers.length; i++)
-        {
-            ZoneModel zone = new ZoneModel(zoneNumbers[i], itemNames[i], percentages[i], initialWeights[i]);
-            adaptor.add(zone);
-        }
+        setOnItemClick();
+        populateList();
 
         return rootView;
     }
+
+    private void populateList() {
+        for (int i=0; i<zoneNumbers.size(); i++)
+        {
+            //ZoneModel zone = new ZoneModel(zoneNumberss[i], itemNames[i], percentages[i], initialWeights[i]);
+            ZoneModel zone = new ZoneModel(zoneNumbers.get(i), itemNames.get(i), percentages.get(i), initialWeights.get(i));
+            adaptor.add(zone);
+        }
+    }
+
+    private void setOnItemClick() {
+        zoneListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
+    private void getData() {
+        String url = "http://99.236.1.225:5001/getzones/1";
+        String result = "";
+        DataFetcher df = new DataFetcher();
+
+        try {
+            result = df.execute(url).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        parseResult(result);
+    }
+
+    private void parseResult(String result) {
+        try{
+            JSONArray zones = new JSONArray(result);
+
+            for (int i=0; i<zones.length(); i++)
+            {
+                int currentWeight = zones.getJSONObject(i).getInt("weight");
+                int initialWeight = zones.getJSONObject(i).getInt("initialweight");
+                int percentage = getPercentage(currentWeight, initialWeight);
+
+                zoneNumbers.add(zones.getJSONObject(i).getInt("id"));
+                itemNames.add(zones.getJSONObject(i).getString("desc"));
+                initialWeights.add(currentWeight);
+                percentages.add(percentage);
+            }
+
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    private int getPercentage(int currentWeight, int initialWeight)
+    {
+        if (initialWeight == 0) { return 0; }
+
+        if (currentWeight >= initialWeight)
+        {
+            return 100;
+        }
+        else
+        {
+            return (int)(((float)currentWeight/(float)initialWeight)*100);
+        }
+    }
+
 }
